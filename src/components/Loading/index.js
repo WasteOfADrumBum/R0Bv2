cb71316330209bbe588454f97103aecb281a060e
Joshua Small <joshua.small@thesocialinst.com>
2021-09-23 23:31:27 -0400

    updated image to relative path

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 const loadingImg =
-  "https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg";
+'/assets/images/loading.svg'
 
 const Loading = () => (
   <div className="spinner">
