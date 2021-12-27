5a9696c2b908b166b3a6b478a51a41640f14ec94
Joshua Small <jmsmall89@gmail.com>
2021-12-27 14:14:51 -0500

    Update server.js
    fix path

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('public/build'))
 }
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
 
 // PORT
