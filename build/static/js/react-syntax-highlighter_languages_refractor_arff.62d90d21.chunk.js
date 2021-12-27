88c06abe1dbbe2da00dd677253bc7bd815a12cb8
Joshua Small <joshua.small@thesocialinst.com>
2021-12-27 12:53:37 -0500

    optimized production build
    npm run build

--- build/static/js/react-syntax-highlighter_languages_refractor_arff.62d90d21.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkr0b_v2_0=self.webpackChunkr0b_v2_0||[]).push([[1438],{7209:function(e){function a(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=a,a.displayName="arff",a.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.62d90d21.chunk.js.map
