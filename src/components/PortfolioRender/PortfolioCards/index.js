3f21381c9588f6908e564fd22c8a6422d6458815
Joshua Small <jmsmall89@gmail.com>
2021-12-28 12:21:56 -0500

    fixed masonry on portfolio page

--- src/components/PortfolioRender/PortfolioCards/index.js
+++ src/components/PortfolioRender/PortfolioCards/index.js
@@ -1,10 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 import moment from 'moment'
-import { Stack, Paper } from '@mui/material/'
-import { Masonry } from '@mui/lab'
-import { styled } from '@mui/material/styles'
 import { deletePortfolio, readPortfolio } from '../../../actions'
 import './_portfolioCards.scss'
 
@@ -47,79 +45,87 @@ const PortfolioCards = ({
           <p>Loading...</p>
         ) : (
           <>
-            {sortedData.map((project, i) => {
-              return (
-                <div className="card m-2" key={`${project} + ${i}`}>
-                  <div className="imgWrapper">
-                    {project.imgSrc ? (
-                      <img
-                        src={`assets/images/portfolio/${project.imgSrc}`}
-                        className="card-img-top"
-                        alt="project_image"
-                      />
-                    ) : (
-                      <img
-                        src="https://via.placeholder.com/300x169"
-                        className="card-img-top"
-                        alt="placeholder"
-                      />
-                    )}
-                  </div>
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      {project.title ? (
-                        <>{project.title}</>
-                      ) : (
-                        <>Untitled Project</>
-                      )}
-                    </h5>
-                    <i className="card-text">
-                      {(project.startDate || project.endDate) && (
-                        <div>
-                          {project.startDate && (
-                            <span className="startDate">
-                              {moment(project.startDate).format('MMMM YYYY')}
-                            </span>
-                          )}
-                          <> - </>
-                          {project.endDate ? (
-                            <span className="endDate">
-                              {moment(project.endDate).format('MMMM YYYY')}
-                            </span>
+            <ResponsiveMasonry
+              columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+            >
+              <Masonry>
+                {sortedData.map((project, i) => {
+                  return (
+                    <div className="card m-2" key={`${project} + ${i}`}>
+                      <div className="imgWrapper">
+                        {project.imgSrc ? (
+                          <img
+                            src={`assets/images/portfolio/${project.imgSrc}`}
+                            className="card-img-top"
+                            alt="project_image"
+                          />
+                        ) : (
+                          <img
+                            src="https://via.placeholder.com/300x169"
+                            className="card-img-top"
+                            alt="placeholder"
+                          />
+                        )}
+                      </div>
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          {project.title ? (
+                            <>{project.title}</>
                           ) : (
-                            <span className="startDate">Current</span>
+                            <>Untitled Project</>
                           )}
-                        </div>
-                      )}
-                    </i>
-                    <h6 className="card-subtitle mb-2 text-muted">
-                      {project.position && <>{project.position}</>}
-                    </h6>
-                    <hr />
-                    <p className="card-text">
-                      {project.description && <>{project.description}</>}
-                    </p>
-                    {project.linkUrl && (
-                      <a
-                        href={`https://${project.linkUrl}`}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="btn btn-outline-primary btn-sm my-3"
-                      >
-                        View
-                      </a>
-                    )}
-                  </div>
-                  <div className="card-footer text-muted">
-                    {project.skills && (
-                      <>
-                        <b>Skills:</b> {project.skills}
-                      </>
-                    )}
-                  </div>
-                </div>
-              )
-            })}
+                        </h5>
+                        <i className="card-text">
+                          {(project.startDate || project.endDate) && (
+                            <div>
+                              {project.startDate && (
+                                <span className="startDate">
+                                  {moment(project.startDate).format(
+                                    'MMMM YYYY',
+                                  )}
+                                </span>
+                              )}
+                              <> - </>
+                              {project.endDate ? (
+                                <span className="endDate">
+                                  {moment(project.endDate).format('MMMM YYYY')}
+                                </span>
+                              ) : (
+                                <span className="startDate">Current</span>
+                              )}
+                            </div>
+                          )}
+                        </i>
+                        <h6 className="card-subtitle mb-2 text-muted">
+                          {project.position && <>{project.position}</>}
+                        </h6>
+                        <hr />
+                        <p className="card-text">
+                          {project.description && <>{project.description}</>}
+                        </p>
+                        {project.linkUrl && (
+                          <a
+                            href={`https://${project.linkUrl}`}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn btn-outline-primary btn-sm my-3"
+                          >
+                            View
+                          </a>
+                        )}
+                      </div>
+                      <div className="card-footer text-muted">
+                        {project.skills && (
+                          <>
+                            <b>Skills:</b> {project.skills}
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  )
+                })}
+              </Masonry>
+            </ResponsiveMasonry>
           </>
         )}
       </>
@@ -136,9 +142,7 @@ const PortfolioCards = ({
         <p>Loading...</p>
       ) : (
         <div className="portfolioCards">
-          <Masonry columns={{ xs: 3, sm: 4 }} spacing={1}>
-            {portfolio && portfolio.length > 0 && <Card />}
-          </Masonry>
+          {portfolio && portfolio.length > 0 && <Card />}
         </div>
       )}
     </>
